d6575194db17e17522e8a78fb6cf9b8e457ad591
Iwandoin <51027505+Iwandoin@users.noreply.github.com>
2021-09-14 18:58:52 +0300

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,7 +11,7 @@ then
     echo "PostgreSQL started"
 fi
 python manage.py collectstatic --noinput
-python manage.py makemigrations
+python manage.py makemigrations todo
 python manage.py migrate
 gunicorn backend.wsgi:application --bind 0.0.0.0:8000
 
