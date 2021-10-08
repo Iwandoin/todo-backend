6c8e16f32e9a26481dc39e47af723920f7a7e954
Iwandoin <51027505+Iwandoin@users.noreply.github.com>
2021-10-08 17:36:08 +0300

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+set -e
+set +x
 export TAG=$(git rev-parse HEAD)
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/l6a6l2j9
 cd backend && docker build -t backendforst:latest .
@@ -7,7 +8,6 @@ docker tag backendforst:latest public.ecr.aws/l6a6l2j9/backendforst:$TAG
 docker push public.ecr.aws/l6a6l2j9/backendforst:$TAG
 cd ..
 ecs-cli configure --cluster Todot --default-launch-type EC2 --region eu-central-1 --config-name configuration
-pwd
 ls
 ecs-cli compose --project-name todot_backend --file td.yml create
 aws ecs update-service --cluster Todot --service todot_service --force-new-deployment --region eu-central-1 --task-definition todot_backend
