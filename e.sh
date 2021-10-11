edf6adb6bed3b15872291345292cf5e3ad5b56f6
Iwandoin <i_kruglik@mail.ru>
2021-10-11 11:05:50 +0300

    Update e.sh

--- e.sh
+++ e.sh
@@ -10,5 +10,6 @@ cd ..
 ecs-cli configure --cluster Todot --default-launch-type EC2 --region eu-central-1 --config-name configuration
 ls
 ecs-cli compose --project-name todot_backend --file td.yml create
-export TD=$(aws ecs list-task-definitions --family-prefix  todot_backend  | jq -r .taskDefinitionArns[-1] | sed 's/.*\///')
+export TD=$(aws ecs list-task-definitions --family-prefix  todot_backend  | jq -r .taskDefinitionArns[-1] )
 aws ecs update-service --cluster Todot --service todot_service --force-new-deployment --region eu-central-1 --task-definition $TD
+#| sed 's/.*\///'
