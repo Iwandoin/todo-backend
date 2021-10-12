e2b6643e2271171a94e8d2a26f5aa7a5ba491e03
Iwandoin <i_kruglik@mail.ru>
2021-10-12 15:44:30 +0300

    Update push.sh

--- push.sh
+++ push.sh
@@ -3,5 +3,6 @@ set -e
 set +x
 export TAG=$(git rev-parse HEAD)
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/l6a6l2j9
+cd backend
 docker tag backendforst:latest public.ecr.aws/l6a6l2j9/backendforst:$TAG
 docker push public.ecr.aws/l6a6l2j9/backendforst:$TAG
