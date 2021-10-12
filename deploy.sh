24ca2ee8b2f2266469ea83fd276cf87d3e29ba89
Iwandoin <i_kruglik@mail.ru>
2021-10-12 15:15:08 +0300

    Make 3 stages

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+set +x
+ecs-cli configure --cluster Todot --default-launch-type EC2 --region eu-central-1 --config-name configuration
+ls
+ecs-cli compose --project-name todot_backend --file td.yml create
+export TaskDefinition=$(aws ecs list-task-definitions --family-prefix  todot_backend --region eu-central-1  | jq -r .taskDefinitionArns[-1] )
+aws ecs update-service --cluster Todot --service todot_service --force-new-deployment --region eu-central-1 --task-definition $TaskDefinition
+TIMEOUT=300
+while [ $i-lt$TIMEOUT ]; do
+  set -e
+  set +x
+  sleep 60
+  export RolloutState=$(aws ecs describe-services --cluster Todot --service todot_service --region eu-central-1 \
+  | jq -r .services[0].deployments[0] | jq -r .rolloutState | grep -e "COMPLETED" || : >/dev/null)
+  echo $(aws ecs describe-services --cluster Todot --service todot_service --region eu-central-1 \
+  | jq -r .services[0].deployments[0] | jq -r .rolloutState) 
+  if [ "$RolloutState" ]; then
+  echo "OK"
+  echo "Service Todot / todot_service updated"
+  echo "Service updated successfully, rollout completed."
+  exit 0
+  fi
+done
